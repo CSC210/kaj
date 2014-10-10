35868b9fa0795cfee6489f1c94c19999774f645b
zzz7zzz <zzz7zzz@users.noreply.github.com>
2014-10-10 08:03:09 -0400

    Delete demo.php

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Create connection
-$con=mysqli_connect("localhost","zzengnin@localhost","Nopointhackingme!128","zzengnin_wheel");
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
