cab53802b2cd45d634f23348ff49dbb302b7c5e7
zzz7zzz <zmzeng8@gmail.com>
2014-10-19 20:17:33 -0400

    Delete unneeded files

--- cgi-bin/remove.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-        setcookie("user", "",time()-3600*30);
-        setcookie("password","",time()-3600*30);
-?>
-
-<script type="text/javascript">
-window.location="../login.php";
-</script>
---
+++ cgi-bin/signupPHP.php
@@ -0,0 +1,34 @@
+<?php
+$email= $_POST["email"];
+$pwd = $_POST["pwd"];
+
+$dbh=mysqli_connect("localhost",
+"zzengnin","Nopointhackingme!128","zzengnin_wheel")
+or die ('I cannot connect to the database.');
+//echo "connected";
+
+$db_password=md5($pwd);
+$sql="INSERT INTO Info(id, pw, email, bs) VALUES('', 
+'$db_password', '$email', '7')";
+
+if(!mysqli_query($dbh, $sql)) {
+
+die('Error:' . mysqli_error($dbh));
+}
+//echo "1 record added";
+mysqli_close($dbh);
+?>
+
+<html>
+<body>
+
+Welcome! Your email is:  <?php echo $_POST["email"]; ?><br>
+Your password
+ is: <?php echo $_POST["pwd"]; ?><br/>
+
+<div> Go on, you know you wanna <a href="../login.php"> log 
+in.</a></div>
+
+</body>
+</html>
+
